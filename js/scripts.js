ccfa50ada198f4034f15ec02af6f6921cc6c3214
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 21:15:58 +0300

    add array to hold correct answers to the survey questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+const csharp = ["different places,different problems", "i wouldn't be here", "round can't fall", "ride it", "probably fir"];
+const java = ["research", "wouldn't be talking to you", "square wouldn't fit", "take to the zoo", "probably cedar"];
+const python = ["practice coding", "why sarcasm", "are they circular?", "take to madagascar", "probably oak"];
 
 $(document).ready(function() {
   $("form#container").submit(function(event) {
@@ -8,7 +11,7 @@ $(document).ready(function() {
     const Q4 = toString($("#q4").val());
     const Q5 = toString($("#q5").val());
 
-  if () {
+  if (Q1, Q2, Q3, Q4, Q5 == csharp) {
     $("#Csharp").show();
   } else if () {
     $("#java").show();
