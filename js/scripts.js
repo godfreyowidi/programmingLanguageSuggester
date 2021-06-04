3e8b1d6f6b16889abc617a9859c29610cad63ec1
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 19:54:00 +0300

    WIP: branching survey results.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+  $("form#container").submit(function(event) {
+    event.preventDefault();
+
+  if (first, last != isString);
+    alert("Please enter a valid name!")
+
+  if () {
+    $("#Csharp").show();
+  } else if () {
+    $("#java").show();
+  } else {
+    $("#python").show();
+  }
+}); 
