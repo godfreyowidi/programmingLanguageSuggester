231f0a7d10e13d5d5825c5382ba8a8e1fc7c015d
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-04 20:11:12 +0300

    add const variable with string converter to js file.

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@
 $(document).ready(function() {
   $("form#container").submit(function(event) {
     event.preventDefault();
-
-  if (first, last != isString);
-    alert("Please enter a valid name!")
+    const Q1 = toString($("#q1").val());
+    const Q2 = toString($("#q2").val());
+    const Q3 = toString($("#q3").val());
+    const Q4 = toString($("#q4").val());
+    const Q5 = toString($("#q5").val());
 
   if () {
     $("#Csharp").show();
@@ -13,4 +15,4 @@ $(document).ready(function() {
   } else {
     $("#python").show();
   }
-}); 
+}) 
