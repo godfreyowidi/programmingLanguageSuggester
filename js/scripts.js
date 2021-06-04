a44ae8723a32a51353fb02209b8db9988b8bcaff
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-05 01:25:33 +0300

    add shifting to radio button with question.

--- js/scripts.js
+++ js/scripts.js
@@ -11,28 +11,7 @@ $(document).ready(function() {
     const Q4 = toString($("#q4").val());
     const Q5 = toString($("#q5").val());
     
-    const result = [Q1, Q2, Q3, Q4, Q5];
-
-    function compare(csharp, result) {
-      csharp.sort();
-      result.sort();
-        
-      if(csharp.length != csharp.length)
-          return false;
-        
-      for(var i = 0; i < csharp.length; i++) {
-          if (csharp[i] != result[i])
-              return false;
-      }
-      $("#Csharp").show();
-      //return true;
-    }
-    function btn() {
-      down.innerHTML = compare(csharp, result);
-    } 
-  })
-})
-    /*if (result === csharp) {
+    if (result === csharp) {
       $("#Csharp").show();
     } else if (result === java) {
       $("#java").show();
@@ -41,4 +20,4 @@ $(document).ready(function() {
     }
     $("#output").text(result);
   }) 
-})*/
+})
