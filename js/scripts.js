18f72fea2be4bf08dffc4daaf6da8389f6637d60
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-05 06:45:56 +0300

    add css rulesets for fonts and background.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,12 @@
-$(document).ready(function(){
-  $("#survey").submit(function(event) {
-    event.preventDefault();
-    function tabulate() {
-      const result = 0;
-      const q1 = document.survey.question1.value;
-      const q2 = document.survey.question2.value;
-      const q3 = document.survey.question3.value;
-      const q4 = document.survey.question4.value;
-      const q5 = document.survey.question5.value;
+function tabulate() {
+  const result = 0;
+  const q1 = document.survey.question1.value;
+  const q2 = document.survey.question2.value;
+  const q3 = document.survey.question3.value;
+  const q4 = document.survey.question4.value;
+  const q5 = document.survey.question5.value;
 
-      if (q1 =="Different place, different problems") {}
-      
-      document.write("hey");
-    }
-  })
-})
+  if (q1 =="Different place, different problems") {}
+  
+  document.write("hey");
+}
