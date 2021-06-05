ee4be97232288ff02d26a11ceb3add909c6c9caf
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-06 01:02:23 +0300

    add branching to display info on different languages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,39 @@
 // Function to check correct answers and display them
-function tabulateResult() {
+function tabulate() {
 
   // count correct answers
-  const correctAnswers = 0;
+  const correctAnswer = [];
 
   // correct input to display csharp
-  const q1 = document.getElementById("q1a").value;
-  const q2 = document.getElementById("q2a").value;
-  const q3 = document.getElementById("q3a").value;
-  const q4 = document.getElementById("q4a").value;
-  const q5 = document.getElementById("q5a").value;
+  const q1 = document.getElementById("q1a").checked;
+  const q2 = document.getElementById("q2a").checked;
+  const q3 = document.getElementById("q3a").checked;
+  const q4 = document.getElementById("q4a").checked;
+  const q5 = document.getElementById("q5a").checked;
 
   // correct input to display java
-  const q5 = document.getElementById("q1b").value;
-  const q6 = document.getElementById("q2b").value;
-  const q7 = document.getElementById("q3b").value;
-  const q8 = document.getElementById("q4b").value;
-  const q9 = document.getElementById("q5b").value;
+  const q6 = document.getElementById("q1b").checked;
+  const q7 = document.getElementById("q2b").checked;
+  const q8 = document.getElementById("q3b").checked;
+  const q9 = document.getElementById("q4b").checked;
+  const q10 = document.getElementById("q5b").checked;
 
   // correct input to display python
-  const q10 = document.getElementById("q1c").value;
-  const q12 = document.getElementById("q2c").value;
-  const q13 = document.getElementById("q3c").value;
-  const q14 = document.getElementById("q4c").value;
-  const q15 = document.getElementById("q5c").value;
+  const q11 = document.getElementById("q1c").checked;
+  const q12 = document.getElementById("q2c").checked;
+  const q13 = document.getElementById("q3c").checked;
+  const q14 = document.getElementById("q4c").checked;
+  const q15 = document.getElementById("q5c").checked;
 
-
+  // Print results (and statement) to document, dependant on outcome
+  if (correctAnswer == q1 && q2 && q3 && q4 && q5) {
+    document.getElementById("csharp").innerHTML;  
+  } else if (correctAnswer == q6 && q7 && q8 && q9 && q10 ) {
+    document.getElementById("java").innerHTML;  
+  } else if (correctAnswer == q11 && q12 && q13 && q14 && q15) {
+    document.getElementById("python").innerHTML;
+  } else {
+    document.getElementById("result").innerHTML = "Sign up at Epicodus for Introduction to Programming";  
+  }  
 }
-$(document).ready(function(){
-  $("form#survey").submit(function(event) {
-    event.preventDefault();
 
-  if (q1 == "Different place, different problems") {result ++}
-  if (q2 == "I wouldn't be talking to you") {result ++}
-  if (q3 == "Aren't they circular") {result ++}
-  if (q4 == "Take it to Madagascar") {result ++}
-  if (q5 == "Maybe Cedar") {result ++}
-  
