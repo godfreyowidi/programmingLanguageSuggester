ec6c4237614dc7757dd77894cf9dd70ed59eeb44
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-05 05:13:54 +0300

    WIP: Refactored html radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,12 @@
+function tabulate() {
+  const result = [];
+  const q1 = document.survey.question1.value;
+  const q2 = document.survey.question2.value;
+  const q3 = document.survey.question3.value;
+  const q4 = document.survey.question4.value;
+  const q5 = document.survey.question5.value;
 
+  if (q1 =="Different place, different problems") {$()}
 
-$(document).ready(function() {
-  $("form#container").submit(function(event) {
-    event.preventDefault();
-    const questions = $("p").each(function() {
-      for (const i = 0; i < questions.length; i += 1) {
-        questions[i].style.display = "none"
-    
-    const Q1 = toString($("#q1").val());
-        
-    if (result === csharp) {
-      $("#Csharp").show();
-    } else if (result === java) {
-      $("#java").show();
-    } else if (result === python) {
-      $("#python").show();
-    }
-  }) 
-})
+  document.write("Hey");
+}
