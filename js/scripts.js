971b81d49a68b781e5877813cf1f2ae375058aef
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-05 18:43:43 +0300

    WIP: working on the logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,19 @@ function tabulate() {
   const q4 = document.survey.question4.value;
   const q5 = document.survey.question5.value;
 
-  if (q1 == "Different place, different problems") {results ++}
-  if (q1 == "I wouldn't be talking to you") {results ++}
-  if (q1 == "Aren't they circular") {results ++}
-  if (q1 == "Take it to Madagascar") {results ++}
-  if (q1 == "Maybe Cedar") {results ++}
+  const outcome = $(document).("#outcome");
+  const survey = document.$("#survey");
+
+  if (q1 == "Different place, different problems") {result ++}
+  if (q2 == "I wouldn't be talking to you") {result ++}
+  if (q3 == "Aren't they circular") {result ++}
+  if (q4 == "Take it to Madagascar") {result ++}
+  if (q5 == "Maybe Cedar") {result ++}
   
-  document.write("hey");
+  $("#survey").hide();
+  if (result <= 3) {
+    result.text("Your result is ${result}. Please try again.");
+  } else {
+    result.text("Your result is ${result}. Good work.");
+  }
 }
