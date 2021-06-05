c512af32756ec5f9909e217a785e991ae07a6a10
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-05 17:31:03 +0300

    WIP: 2nd day-add assignment of correct answers.

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,11 @@ function tabulate() {
   const q4 = document.survey.question4.value;
   const q5 = document.survey.question5.value;
 
-  if (q1 =="Different place, different problems") {}
+  if (q1 == "Different place, different problems") {results ++}
+  if (q1 == "I wouldn't be talking to you") {results ++}
+  if (q1 == "Aren't they circular") {results ++}
+  if (q1 == "Take it to Madagascar") {results ++}
+  if (q1 == "Maybe Cedar") {results ++}
   
   document.write("hey");
 }
