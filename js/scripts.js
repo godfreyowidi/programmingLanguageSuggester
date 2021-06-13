25b9085aa8842baf9995e89558d9fabb7b12cfb1
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-13 18:16:03 +0300

    add conditional to handle question 2 - 5 using answer input options.

--- js/scripts.js
+++ js/scripts.js
@@ -10,4 +10,70 @@ $(document).ready(function() {
     let totalScore = questionOne(q1Input) + questionTwo(q2Input) + questionThree(q3Input) + questionFour(q4Input) + questionFive(q5Input);
     $(".result").text(outcome(totalScore));
   });
+
+
+  // to handle question-1
+  function questionOne(q1Input){
+    if (q1Input === "Different place, different problems") {
+      return 1;
+    }
+    else if (q1Input === "Research"){
+      return 2;
+    }
+    else if (q1Input === "Practice coding"){
+      return 3;
+    }
+  }
+
+  // to handle question-2
+  function questionTwo(q2Input){
+    if (q2Input === "I wouldn't be here"){
+      return 1;
+    }
+    else if (q2Input === "I wouldn't be talking to you"){
+      return 2;
+    }
+    else if (q2Input === "Why Sarcasm?"){
+      return 3;
+    }
+  }
+
+  // handle question-3
+  function questionThree(q3Input){
+    if (q3Input === "Round can't fall"){
+      return 1;
+    }
+    else if (q3Input === "Squares can't fit"){
+      return 2;
+    }
+    else if (q3Input === "Aren't they circular") {
+      return 3;
+    }    
+  }
+
+  // handle question-4
+  function questionFour(q4Input){
+    if (q4Input === "Ride it"){
+      return 1;
+    }
+    else if (q4Input === "Take it to the Zoo"){
+      return 2;
+    }
+    else if (q4Input === "Take it to Madagascar") {
+      return 3;
+    }
+  }
+
+  // handle question-5
+  function questionFive(q5Input){
+    if (q5Input === "Probably Fir"){
+      return 1;
+    }
+    else if (q5Input === "Maybe Cedar"){
+      return 2;
+    }
+    else if (q5Input === "Definately Oak") {
+      return 3;
+    }    
+  }
 });
