2a44d9e07193f199bb4b858eb4e74b701a4c22b5
Godfrey Owidi <godfreyowiidi@gmail.com>
2021-06-13 17:19:46 +0300

    add 5 variables for answers input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,71 +1,13 @@
-$(document).ready(function () {
-  $("input:radio[name='question']").click(function () {
-      if ($("#1a, #1b, #1c, #1d, #1e").is(":checked")) {
-          $("#csharp").show();
-      } else {
-          $("#csharp").hide();
-      };    
+$(document).ready(function() {
+  $("button").click(function() {
+    event.preventDefault();
+    let name = $("#name").val();
+    let q1Input = $("#question1").val();
+    let q2Input = $("#question2").val();
+    let q3Input = $("#question3").val();
+    let q4Input = $("#question4").val();
+    let q5Input = $("#question5").val();
+    let totalScore = questionOne(q1Input) + questionTwo(q2Input) + questionThree(q3Input) + questionFour(q4Input) + questionFive(q5Input);
+    $(".result").text(outcome(totalScore));
   });
 });
-
-$(document).ready(function () {
-  $("input:radio[name='question']").click(function () {
-      if ($("#2a, #2b, #2c, #2d, #2e").is(":checked")) {
-          $("#java").show();
-      } else {
-          $("#java").hide();
-      }
-  });
-});
-
-$(document).ready(function () {
-  $("input:radio[name='question']").click(function () {
-      if ($("#3a, #3b, #3c, #3d, #3e").is(":checked")) {
-          $("#python").show();
-      } else {
-          $("#python").hide();
-      }
-  });
-});
-
-/*// Function to check correct answers and display them
-function tabulate() {
-
-  // count correct answers
-  const correctAnswer = [];
-
-  // correct input to display csharp
-  const q1 = document.getElementById("q1a").checked;
-  const q2 = document.getElementById("q2a").checked;
-  const q3 = document.getElementById("q3a").checked;
-  const q4 = document.getElementById("q4a").checked;
-  const q5 = document.getElementById("q5a").checked;
-
-  // correct input to display java
-  const q6 = document.getElementById("q1b").checked;
-  const q7 = document.getElementById("q2b").checked;
-  const q8 = document.getElementById("q3b").checked;
-  const q9 = document.getElementById("q4b").checked;
-  const q10 = document.getElementById("q5b").checked;
-
-  // correct input to display python
-  const q11 = document.getElementById("q1c").checked;
-  const q12 = document.getElementById("q2c").checked;
-  const q13 = document.getElementById("q3c").checked;
-  const q14 = document.getElementById("q4c").checked;
-  const q15 = document.getElementById("q5c").checked;
-
-  // Print results (and statement) to document, dependant on outcome
-  if (correctAnswer == q1 && q2 && q3 && q4 && q5) {
-    document.getElementById("csharp").innerHTML;  
-  } else if (correctAnswer == q6 && q7 && q8 && q9 && q10 ) {
-    document.getElementById("java").innerHTML;  
-  } else if (correctAnswer == q11 && q12 && q13 && q14 && q15) {
-    document.getElementById("python").innerHTML;
-  } else {
-    document.getElementById("result").innerHTML = "Sign up at Epicodus for Introduction to Programming course";  
-  }
-  
-  // Reset 
-  document.getElementById("survey").reset();
-}*/
